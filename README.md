a9bfdc1c3cc8c9daaadf7e77521d1a6ca1d0440b
Bill Columbia <hello@billcolumbia.com>
2013-04-02 11:14:31 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mullet App
+
+To do: 
+
+Authenticate with Twitter
+Boxes Model
+Boxes Controller and Views
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MulletApp::Application.config.secret_token = 'cf07d6468cf8b63d4b2c24fdd2673e70082ea52534fdebc42bd15c6640993d041259effd5291b2f05ad7ac3dd6729d9b0bbc606ce3260ad186e16f2eeac520e7'
